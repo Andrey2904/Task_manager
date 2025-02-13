e3be8e2271f65aed2004785547a5ea89ca5d2b5f
Andrew Svyazov <svyazov.andrei@yandex.ru>
2025-02-13 21:03:55 +0300

    add Kill process

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -38,6 +38,37 @@ namespace ConsoleApp2
                 {
                     Console.WriteLine("Процессы с таким именем не найдены.");
                 }
+                Console.Write("Введите ID или имя процесса для завершения: ");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out int processId))
+                {
+                    var processToKill = allProcesses.FirstOrDefault(p => p.Id == processId);
+                    if (processToKill != null)
+                    {
+                        processToKill.Kill();
+                        Console.WriteLine($"Процесс {processToKill.ProcessName} (ID: {processId}) завершен.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Процесс с таким ID не найден.");
+                    }
+                }
+                else
+                {
+                    var processesToKill = allProcesses.Where(p => p.ProcessName.IndexOf(input, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+                    if (processesToKill.Any())
+                    {
+                        foreach (var proc in processesToKill)
+                        {
+                            proc.Kill();
+                            Console.WriteLine($"Процесс {proc.ProcessName} (ID: {proc.Id}) завершен.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Процессы с таким именем не найдены.");
+                    }
+                }
             }
             catch (Exception ex)
             {
