37161539b18a6d822cb697e6b4d9dde3b18ab0a7
Andrew Svyazov <svyazov.andrei@yandex.ru>
2025-02-13 20:47:33 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Process currentProcess = Process.GetCurrentProcess();
+                Process[] allProcesses = Process.GetProcesses();
+
+                Console.WriteLine("Список запущенных процессов:");
+                foreach (Process process in allProcesses)
+                {
+                    string marker = process.Id == currentProcess.Id ? "<-- текущее приложение" : "";
+                    Console.WriteLine($"{process.ProcessName} (ID: {process.Id}) {marker}");
+                }
+
+                Console.Write("Введите имя процесса для поиска его ID: ");
+                string processName = Console.ReadLine();
+                var matchingProcesses = allProcesses.Where(p => p.ProcessName.IndexOf(processName, StringComparison.OrdinalIgnoreCase)>=0).ToList();
+                if (matchingProcesses.Any())
+                {
+                    Console.WriteLine("Найденные процессы:");
+                    foreach (var proc in matchingProcesses)
+                    {
+                        Console.WriteLine($"{proc.ProcessName} (ID: {proc.Id})");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Процессы с таким именем не найдены.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+    }
+}
